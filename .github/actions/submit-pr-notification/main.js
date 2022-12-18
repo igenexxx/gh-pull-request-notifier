146202017ffdac4925b9f1d4aa2f756a9277f5f8
Zhenya Sikirzhitsky <zhenya_sikirzhitsky@epam.com>
2022-12-18 23:59:05 +0200

    pr-one: check user

--- .github/actions/submit-pr-notification/main.js
+++ .github/actions/submit-pr-notification/main.js
@@ -6,6 +6,6 @@ const getTeamsMessage = require("./getTeamsMessage");
 // console.log(github.context);
 
 (async () => {
-    console.log(github.context.payload.pull_request.user);
+    console.log(JSON.stringify(github.context, null, 2));
     // await axios.post(process.env.TEAMS_WEBHOOK_URL, getTeamsMessage(github.context.payload));
 })();
